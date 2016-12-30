aa79bfb84a9160da85a1f983287a8e164d614227
Charles Massry <me@charliemassry.com>
2016-12-30 16:32:47 -0500

    Add full request fix

--- lib/pipedrive/base.rb
+++ lib/pipedrive/base.rb
@@ -89,6 +89,7 @@ module Pipedrive
         if res.ok?
           data = res['data'].nil? ? [] : res['data'].map{|obj| new(obj)}
           if get_absolutely_all && res['additional_data']['pagination'] && res['additional_data']['pagination'] && res['additional_data']['pagination']['more_items_in_collection']
+            options[:query] ||= {}
             options[:query] = options[:query].merge({:start => res['additional_data']['pagination']['next_start']})
             data += self.all(nil,options,true)
           end
